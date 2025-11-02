735c3521527d12c29b58074d08999c04be049831
mr_tomate <mr_tomate2024@hotmail.com>
2025-11-02 18:43:43 -0400

    feat: Add products and public routes

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,15 @@
+
+import authRouter from "@/routes/auth.routes";
+import productsRouter from "@/routes/products.routes";
+import publicRoutes from "@/routes/public.routes";
 import express from "express";
-
-import authRouter from "./routes/auth.routes";
-
 const app = express();
 
 app.use(express.json());
 
 app.use("/api/auth", authRouter);
-
+app.use("/api/products", productsRouter);
+app.use("/api/public", publicRoutes);
 app.get("/", (_req, res) => {
 	res.json({ message: "Bem Vindo a Api Doce Ponto" });
 });
