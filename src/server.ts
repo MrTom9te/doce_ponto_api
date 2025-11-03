66294c6a9574d11d51b73328533876ab44689cf7
mr_tomate <mr_tomate2024@hotmail.com>
2025-11-02 21:58:57 -0400

    feat: Add logging middleware and static image serving
    Adds a logging middleware to log incoming requests and serves static
    images from the public/images directory.

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,10 @@
-
+import path from "node:path";
+import express from "express";
+import morgan from "morgan";
 import authRouter from "@/routes/auth.routes";
 import productsRouter from "@/routes/products.routes";
 import publicRoutes from "@/routes/public.routes";
-import express from "express";
+
 const app = express();
 
 app.use(express.json());
@@ -13,6 +15,15 @@ app.use("/api/public", publicRoutes);
 app.get("/", (_req, res) => {
 	res.json({ message: "Bem Vindo a Api Doce Ponto" });
 });
+app.use((req, res, next) => {
+  console.log(`Acessando: ${req.method} ${req.path}`);
+  next(); // passa para o próximo middleware/rota
+});
+
+app.use(
+	"/static/images",
+	express.static(path.join(__dirname, "..", "public", "images")),
+);
 
 const PORT = process.env.PORT || 3000;
 
