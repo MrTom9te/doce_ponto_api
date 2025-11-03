8f38e62087f036c0ebe129518a96c980b5196ebc
mr_tomate <mr_tomate2024@hotmail.com>
2025-11-02 21:59:41 -0400

    feat: Use base64 for product image
    Replaced imageUrl with imageBase64 in CreateProductRequest and
    UpdateProductRequest interfaces.

--- src/types/products.types.ts
+++ src/types/products.types.ts
@@ -27,14 +27,14 @@ export interface CreateProductRequest {
 	name: string;
 	description: string;
 	price: number;
-	imageUrl?: string;
+	imageBase64: string;
 }
 
 export interface UpdateProductRequest {
 	name?: string;
 	description?: string;
 	price?: number;
-	imageUrl?: string;
+	imageBase64?: string;
 }
 
 export interface ToggleProductRequest {
