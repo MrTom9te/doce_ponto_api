d00873d178713b3671f384c527f22e9c5ed5c6d5
mr_tomate <mr_tomate2024@hotmail.com>
2025-11-04 17:34:10 -0400

    Update database connection string
    Use environment variable for database URL.

--- prisma.config.ts
+++ prisma.config.ts
@@ -1,12 +1,12 @@
 import { defineConfig } from "prisma/config";
 
 export default defineConfig({
-	schema: "prisma/schema.prisma",
-	migrations: {
-		path: "prisma/migrations",
-	},
-	engine: "classic",
-	datasource: {
-		url: "postgresql://doce_ponto:senha123@localhost:5432/doce_ponto",
-	},
+  schema: "prisma/schema.prisma",
+  migrations: {
+    path: "prisma/migrations",
+  },
+  engine: "classic",
+  datasource: {
+    url: "postgresql://neondb_owner:npg_gRDY4JmWcn5C@ep-crimson-recipe-ahd7rp1a-pooler.c-3.us-east-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require",
+  },
 });
