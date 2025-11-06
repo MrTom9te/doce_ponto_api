d1adb7e13822aedefdd44b7f60dfd845a05fdcd3
mr_tomate <mr_tomate2024@hotmail.com>
2025-11-06 19:31:51 -0400

    feat: Implement Swagger documentation
    Adds Swagger documentation to the API, allowing for better understanding
    of endpoints and their usage.

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,18 @@
 import path from "node:path";
 import express from "express";
+import swaggerJsdoc from "swagger-jsdoc";
+import swaggerUi from "swagger-ui-express";
 import authRouter from "@/routes/auth.routes";
 import ordersRouter from "@/routes/order.routes";
+import paymentsRouter from "@/routes/payments.routes";
 import productsRouter from "@/routes/products.routes";
 import publicRoutes from "@/routes/public.routes";
-import paymentsRouter from "@/routes/payments.routes";
+import swaggerOptions from "../swagger.config";
+
 const app = express();
+
+const swaggerDocs = swaggerJsdoc(swaggerOptions);
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocs));
 
 app.use(express.json());
 
