6bde657430952fe39bffa3a2ca0aebe7cf227d1f
mr_tomate <mr_tomate2024@hotmail.com>
2025-11-07 12:27:14 -0400

    Refactor: Configure Swagger documentation
    Provide basic Swagger configuration directly in the file. This includes
    the title, version, description, and base URL for the API documentation.

--- src/server.ts
+++ src/server.ts
@@ -7,11 +7,27 @@ import ordersRouter from "@/routes/order.routes";
 import paymentsRouter from "@/routes/payments.routes";
 import productsRouter from "@/routes/products.routes";
 import publicRoutes from "@/routes/public.routes";
-import swaggerOptions from "../swagger.config";
 
 const app = express();
 
-const swaggerDocs = swaggerJsdoc(swaggerOptions);
+const swaggerDocs = swaggerJsdoc({
+  swaggerDefinition: {
+    openapi: "3.0.0",
+    info: {
+      title: "Doce Ponto API",
+      version: "1.0.0",
+      description:
+        "Documentação da API para o sistema de confeitaria Doce Ponto, gerada automaticamente.",
+    },
+    servers: [
+      {
+        url: "http://localhost:3000/api", // URL base da sua API
+      },
+    ],
+  },
+
+  apis: ["./src/routes/*.ts"],
+});
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocs));
 
 app.use(express.json());
